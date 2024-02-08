aabdbb62291afcdfab78c5515ff64ec57883e579
hackerFaruk <onuralparslan97@gmail.com>
2024-02-09 00:07:08 +0300

    api added

---
+++ requestHandler.js
@@ -0,0 +1,34 @@
+
+
+  // Function to handle the button click event
+  function getName() {
+   
+    // if fetch gets cors error 
+    // npm install cors
+    // Send a GET request to the API endpoint
+    fetch('https://paranceys-cool-api.cyclic.app/name')
+        .then(response => {
+            // Check if the response is successful
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Parse the JSON response
+            return response.json();
+        })
+        .then(data => {
+            // Update the paragraph element with the random number
+            document.getElementById('apiNameAnswer').textContent = `Api answer is : ${data}`;
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error fetching data:', error);
+        });
+}
+
+
+
+// dom dan eventi dinlemeye başladın clicke func cağırdın signal slot mantığı 
+
+
+// Add event listener to the button
+document.getElementById('apiNameButton').addEventListener('click', getName);
